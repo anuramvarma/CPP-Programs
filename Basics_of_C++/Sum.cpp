1dc5d88e290270c5f576476923adc90a5c9eb9d1
Anuram Prakash Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-08-13 19:06:04 +0530

    Update Sum.cpp

--- Basics_of_C++/Sum.cpp
+++ Basics_of_C++/Sum.cpp
@@ -4,7 +4,7 @@ int main()
 {
     int a = 2;
     int b = 33;
-    int result = a + b;
-    cout << result << endl;
+    float result = a+b;
+    cout << "Result is : "<<(result) << endl;
     return 0;
 }
