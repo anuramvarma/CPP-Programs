46c13da34806dd7fb72db7b707034053dc5bae99
Anuram Prakash Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-08-16 17:46:42 +0530

    Create Arthimatic.cpp

---
+++ Basics_of_C++/Operators/Arthimatic.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;	
+int main ()
+{
+	int a;
+	int b;
+	cout << "Enter value of A : ";
+		cin >> a;
+	cout << "Enter value of B : ";	
+		cin >> b;
+	cout << "Addition :" << (a+b)<< endl;
+	cout << "Substraction :" <<(a-b) <<endl;
+	cout << "Multiplication :" << (a*b)<< endl;
+	cout << "Division :" << (a/b)<<"\n";
+	cout << "Modulo Division :" << (a%b)<<"\n";
+}
+
