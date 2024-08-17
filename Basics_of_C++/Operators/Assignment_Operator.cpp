d4a1bbb079dbfca5a0918eb3a1e2c3fd34e38aa2
Anuram Prakash Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-08-17 22:29:49 +0530

    Create Assignment_Operator.cpp

---
+++ Basics_of_C++/Operators/Assignment_Operator.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;	
+int main ()
+{
+	int a;
+	int b;
+    cout << "---- Executing Assignment Operations !! ---- "<<endl;
+	cout << "Enter value of A : ";
+		cin >> a;
+	cout << "Enter value of B : ";	
+		cin >> b;
+	cout << "Addition :" << (a+=b)<< endl;
+	cout << "Substraction :" <<(a-=b) <<endl;
+	cout << "Multiplication :" << (a*=b)<< endl;
+	cout << "Division :" << (a/=b)<<"\n";
+	cout << "Modulo Division :" << (a%=b)<<"\n";
+}
+
