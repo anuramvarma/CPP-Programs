7a7bab71da085ff7a076ab8133350957a4816e12
Anuram Prakash Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-09-07 11:43:51 +0530

    Create Upper_Lower.cpp

---
+++ Basics_of_C++/Conditional_Statements/Upper_Lower.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    char ch;
+    cout<<"Enter a character : ";
+    cin>>ch;
+    if(ch>='a'&&ch>='z')
+    {
+        cout<<"It is in Lower case ";
+    }
+    else
+    {
+        cout<<"It is in UPPER CASE";
+    }
+}
