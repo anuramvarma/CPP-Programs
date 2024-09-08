ec065b3f88995d80827cf4eb90c8a3a59bc1a97a
Anuram Prakash Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-09-08 20:52:39 +0530

    Create Numbers.cpp

---
+++ Basics_of_C++/Loops/While/Numbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+  int count=1;
+  cout<<"Numbers : ";
+  while(count<=10)
+  {
+    cout<<count<<" ";
+    count++;
+  }
+  cout<<endl;
+}
